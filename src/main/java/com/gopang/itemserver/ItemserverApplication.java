d224fc6a0e28109591ccf769a12a0ff129d94843
jhcode33 <jhcode33@gmail.com>
2023-11-21 13:35:58 +0900

    Chore: Add Dependency

--- src/main/java/com/gopang/itemserver/ItemserverApplication.java
+++ src/main/java/com/gopang/itemserver/ItemserverApplication.java
@@ -2,8 +2,13 @@ package com.gopang.itemserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
+@EnableDiscoveryClient
+//@EnableFeignClients
 public class ItemserverApplication {
 
     public static void main(String[] args) {
